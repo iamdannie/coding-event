46c74ba008043a53794fd0e2c872203ba92b6c29
iamdannie <iamtaiyco@gmail.com>
2021-02-15 19:26:15 -0600

    Initial commit

---
+++ src/test/java/org/launchcode/codingevent/CodingEventApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
